5a2a41b8e2e96f2037d6d4eb9297fb3f5f219e66
LeeMoofon0222 <moofon0222@gmail.com>
2024-02-23 16:07:09 +0800

    Video input

--- Detect.py
+++ Detect.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("best.pt")
+model.conf = 0.2
 result = model.predict(
     #source="C:/Users/Moofon/桌面/髒污圖片二/images - 2024-01-21T024827.048.jpg",
     source="C:/Users/Moofon/桌面/input.mp4",
