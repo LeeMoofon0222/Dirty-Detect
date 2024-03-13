eb34e20010eedb9ac1a1ccad58b8236176542d27
LeeMoofon0222 <moofon0222@gmail.com>
2024-03-14 01:18:35 +0800

    Upload new model

--- Detect.py
+++ Detect.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
 
-model = YOLO("best(100).pt")
+model = YOLO("best(141,64).pt")
 
-#source="C:/Users/Moofon/桌面/髒污圖片二/images - 2024-01-21T024827.048.jpg"
-#source="C:/Users/Moofon/桌面/input.mp4"
-#source="C:/Users/Moofon/桌面/髒污圖片二/images - 2024-01-21T024327.665.jpg"
-source="C:/Users/Moofon/桌面/髒污圖片五/images (54).jpg"
-#source="C:/Users/Moofon/桌面/髒污圖片二/images - 2024-01-21T024345.459.jpg"
-#result = model.predict(source,mode="predict", save=True, save_dir=save_dir,device=0)
+#source="C:/Users/Moofon/桌面/髒污圖片 全/髒污圖片三/images - 2024-01-21T030406.547.jpg"
+#source="C:/Users/Moofon/桌面/髒污圖片 全/髒污圖片/images - 2024-01-21T020628.310.jpg"
+#source="C:/Users/Moofon/桌面/髒污圖片 全/髒污圖片/images (59).jpg"
+#source="C:/Users/Moofon/桌面/髒污圖片 全/髒污圖片二/images - 2024-01-21T024827.048.jpg"
+#source="C:/Users/Moofon/桌面/髒污圖片 全/髒污圖片三/images (8).jpg"
+source="C:/Users/Moofon/桌面/髒污圖片 全/input.mp4"
 
-results = model(source, mode="predict", conf = 0.2,save_crop=True, name="C:/Users/Moofon/桌面/Output", exist_ok=True, device=0)
+result = model.predict(source,mode="predict", save=True, name="C:/Users/Moofon/桌面/Output/64", show_labels=False,show_conf=False ,conf=0.3,device=0,exist_ok=True)
 
